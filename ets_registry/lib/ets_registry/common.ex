6562790191536f317aa0b673c265abb604dd2833
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2015-07-02 22:34:10 +0300

    [ets_registry] Elimanate Common.pid

--- ets_registry/lib/ets_registry/common.ex
+++ ets_registry/lib/ets_registry/common.ex
@@ -1,18 +1,9 @@
 defmodule EtsRegistry.Common do
   defmacro __using__(_) do
     quote do
-      @spec pid() :: pid
-      def pid() do
-        GenServer.call(__MODULE__, :pid)
-      end
-
       @spec state() :: term
       def state() do
         GenServer.call(__MODULE__, :state)
-      end
-
-      def handle_call(:pid, _, state) do
-        {:reply, self(), state}
       end
 
       def handle_call(:state, _, state) do
